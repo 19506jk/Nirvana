e2b6098bf7bdf9b9da3599b287a21430988c7dfa
19506jk <pengdanny23@gmail.com>
2014-06-28 19:32:00 -0400

    Bump to version 0.1.1

--- src/cc3k/App.java
+++ src/cc3k/App.java
@@ -8,7 +8,7 @@ import town.MainInteraction;
 
 
 /*
- * CC3K Ver 0.1.0
+ * CC3K Ver 0.1.1
  * Danny & Louis version
  * This is the main App class, still under construction
  */
@@ -248,7 +248,7 @@ public class App {
 		for (int x = 0; x < 50; x++)
 			System.out.println('\n');
 
-		System.out.println("Welcome to CC3K ver 0.1.0");
+		System.out.println("Welcome to CC3K ver 0.1.1");
 		System.out.println("Choose your race:");
 		System.out.println("Human(h), Elves(e), Dwarf(d), Orc(o)");
 		System.out.println();
